5d81c5b63e73fd69c0ce6d2a692ba7e992cd9f98
JulianDiani <juliandiani95@gmail.com>
2022-05-04 18:30:04 -0300

    borre y agregue todo denuevo xq no se que rompi al intentar pushear antes xD

--- api/routes/carreras.js
+++ api/routes/carreras.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var models = require("../models");
 
 router.get("/", (req, res) => {
-  console.log("Esto es un mensaje para ver en consolaaa");
+  console.log("Esto es un mensaje para ver en consola");
   models.carrera
     .findAll({
       attributes: ["id", "nombre"]
