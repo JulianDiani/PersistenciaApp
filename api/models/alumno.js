f75ffc9ba22588827c47d95b9214720a7d860e11
JulianDiani <juliandiani95@gmail.com>
2022-06-26 13:15:26 -0300

    Agregado alumnos y cambios chicos

---
+++ api/models/alumno.js
@@ -0,0 +1,18 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const alumno = sequelize.define('alumno', {
+    nombre: DataTypes.STRING,
+    dni: DataTypes.STRING,
+    id_carrera: DataTypes.INTEGER
+  }, {});
+  alumno.associate = function(models) {
+    // associations can be defined here
+    alumno.belongsTo(models.carrera, 
+      { 
+        as: "Carrera-Relacionada", 
+        foreignKey: "id_carrera"
+      })
+
+  };
+  return alumno;
+};
